6ebc2942bacd63515818606762e1f351ed68f96d
Alican Çevik <alican.cevik@hotmail.com>
2022-01-09 17:03:52 +0300

    initial commit

---
+++ NetCoreApiSample/DataContext/NetCoreApiSampleDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreApiSample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreApiSample.DataContext
+{
+    public class NetCoreApiSampleDataContext : DbContext
+    {
+        public NetCoreApiSampleDataContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //  MultipleActiveResultSets=True
+
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0QQF01N; Initial Catalog=NetCoreApiSampleDb; User Id=sa; Password=123123123;",
+                builder => builder.EnableRetryOnFailure());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ NetCoreApiSample/Dtos/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreApiSample.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
